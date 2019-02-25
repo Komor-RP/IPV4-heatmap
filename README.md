8c0ea592676a73781c267cfb11be0f589f3ce727
Fan Huang <fan.huang.33@gmail.com>
2019-02-25 10:00:06 -0500

    parses database with frequency

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # IPV4-heatmap
+
+## Development Notes
+Running sql file on the command line
+```shell
+psql -U postgres -d postgres -a -f ./loadData.sql
+```
--- loadData.sql
+++ loadData.sql
@@ -1,9 +1,9 @@
-CREATE TABLE Addresses (
-    id SERIAL NOT NULL,
-    latitude FLOAT  NOT NULL,
-    longitude FLOAT  NOT NULL
-    
-);
+-- CREATE TABLE Addresses (
+--     id SERIAL NOT NULL,
+--     latitude FLOAT  NOT NULL,
+--     longitude FLOAT  NOT NULL,
+--     frequency INTEGER NOT NULL
+-- );
 
 -- Create temporary table for all the data
 CREATE TEMP TABLE tmp (
@@ -19,11 +19,15 @@ CREATE TEMP TABLE tmp (
     extra_column8 FLOAT
 );
 
-\copy tmp
-FROM './Geolite2-City-Blocks-IPv4.csv' CSV HEADER;
+\copy tmp FROM './Geolite2-City-Blocks-IPv4.csv' CSV HEADER;
 
+-- get the frequency of unique longitude and latitude rows
 -- copy over the latitude and longitude columns
 -- where latitude and longitude are not null
-INSERT INTO addresses (latitude, longitude)
-SELECT latitude, longitude FROM tmp
-WHERE latitude is NOT NULL AND longitude is NOT NULL;
+
+INSERT INTO addresses (latitude, longitude, frequency)
+SELECT latitude, longitude, Count(*) as frequency
+FROM tmp
+WHERE latitude IS NOT NULL
+AND longitude is NOT NULL
+GROUP BY latitude, longitude;
