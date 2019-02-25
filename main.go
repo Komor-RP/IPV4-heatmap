13754f2d041a732b8c7424f257fbc48c310d085e
Fan Huang <fan.huang.33@gmail.com>
2019-02-24 22:28:42 -0500

    database connection working

--- main.go
+++ main.go
@@ -1,26 +1,81 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 var addresses []Location
 
 // http://localhost:8080/?top=23.5&bottom=50.2&left=20&right=-20
 func main() {
-	addresses = append(addresses, Location{ID: 1, Latitude: 50.2, Longitude: 20})
-	addresses = append(addresses, Location{ID: 2, Latitude: 29, Longitude: -320})
-	addresses = append(addresses, Location{ID: 3, Latitude: -45, Longitude: 120})
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	host := os.Getenv("db_host")
+	port := os.Getenv("db_port")
+	user := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	dbname := os.Getenv("db_name")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	fmt.Println(psqlInfo)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStatement := `
+	SELECT * FROM addresses WHERE
+	latitude > ($1) AND latitude < ($2)`
+	rows, err := db.Query(sqlStatement, 20, 30)
+
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var point Location
+		err = rows.Scan(&point.ID, &point.Latitude, &point.Longitude)
+		if err != nil {
+			panic(err)
+		}
+		addresses = append(addresses, point)
+	}
+
+	fmt.Println(addresses[0])
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", testHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
+}
+
+func makeQuery(top, bottom, left, right string) {
+	// sqlStatement := `
+	// SELECT * FROM addresses WHERE
+	// latitude > ($1)`
+	// _, err = db.Exec(sqlStatement, 20)
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
